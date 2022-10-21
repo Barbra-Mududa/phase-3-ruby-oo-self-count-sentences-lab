c1861a107951bfed00d510eeabb9280334ae684c
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-10-21 22:40:01 +0300

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
 require 'pry'
 
+# monkey patching.
+# The practice of adding methods to or altering Ruby's built-in classes 
+#considered carefully before use
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?] /).count
   end
 end
